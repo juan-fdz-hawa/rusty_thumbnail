4072deaeff4ff885847472b415d1cdf8da098298
Juan Fernandez <juan@getmyboat.com>
2024-11-13 08:17:08 -0400

    Wire up DB access

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    dotenv::dotenv()?;
+    let db_url = std::env::var("DATABASE_URL")?;
+
+    let db_pool = sqlx::SqlitePool::connect(&db_url).await?;
+
+    sqlx::migrate!("./migrations").run(&db_pool).await?;
+
     Ok(())
 }
